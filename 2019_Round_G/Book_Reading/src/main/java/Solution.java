06fde74fd23f160172154e2269b88bc2c1e5ecaa
Lecseruz <magomedalievic@gmail.com>
2019-10-24 14:35:08 +0300

    update

--- 2019_Round_G/Book_Reading/src/main/java/Solution.java
+++ 2019_Round_G/Book_Reading/src/main/java/Solution.java
@@ -1,5 +1,51 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Solution
 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int cs = 1; cs <= t; ++cs)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int q = sc.nextInt();
+            int[] pages = new int[n + 1];
+            Arrays.fill(pages, 1);
+            int r1;
+            for (int i = 0; i < m; ++i)
+            {
+                r1 = sc.nextInt();
+                pages[r1] = 0;
+            }
+            Map<Integer, Long> map = new HashMap<>();
+            for (int i = 0; i < q; ++i)
+            {
+                r1 = sc.nextInt();
+                map.computeIfPresent(r1, (k, v) -> ++v);
+                map.putIfAbsent(r1, (long)1);
+            }
+            long ans = 0;
+            for (Map.Entry it : map.entrySet())
+            {
+                Long r2 = (Long)it.getValue();
+                r1 = (Integer)it.getKey();
+                long cnt = 0;
+                if (r2 != 0)
+                {
+                    for (int i = r1; i <= n; i += r1)
+                    {
+                        if (pages[i] != 0)
+                            ++cnt;
+                    }
+                    ans += (cnt * r2);
+                }
+            }
+            System.out.println("Case #" + cs + ": " + ans);
+        }
+    }
 }
--- 2019_Round_G/Equation/src/main/java/Solution.java
+++ 2019_Round_G/Equation/src/main/java/Solution.java
@@ -1,5 +1,114 @@
-package PACKAGE_NAME;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution
 {
+    public static void main(String[] args)
+    {
+        try (Scanner sc = new Scanner(System.in))
+        {
+            long[] two = new long[55];
+            long store = 1;
+            int mx = 50;
+            for (int i = 0; i < 55; ++i)
+            {
+                two[i] = store;
+                store *= 2;
+            }
+            int tests = sc.nextInt();
+            for (int cs = 1; cs <= tests; ++cs)
+            {
+                int n = sc.nextInt();
+                long m = sc.nextLong();
+                long[] one = new long[55];
+                long[] zero = new long[55];
+                long[] suf = new long[55];
+                Arrays.fill(one, 0);
+                Arrays.fill(zero, 0);
+                Arrays.fill(suf, 0);
+                for (int i = 0; i < n; ++i)
+                {
+                    long r1 = sc.nextLong();
+                    for (int j = 0; j < mx; ++j)
+                    {
+                        if ((r1 & two[j]) != 0)
+                        {
+                            one[j] += two[j];
+                        }
+                        else
+                            zero[j] += two[j];
+                    }
+                }
+                for (int i = 0; i < mx; ++i)
+                {
+                    suf[i] = Math.min(one[i], zero[i]);
+                }
+                for (int i = 0; i < mx; ++i)
+                {
+                    suf[i + 1] += suf[i];
+                }
+                // DB(suf[50]);
+                if (suf[50] > m)
+                {
+                    System.out.println("Case #" + cs + ": -1");
+                    continue;
+                }
+                long sum = 0, ans = 0;
+                for (int i = mx - 1; i >= 0; --i)
+                {
+                    long infront = 0;
+                    if (i >= 1)
+                        infront = suf[i - 1];
+                    if (zero[i] + sum + infront <= m)
+                    {
+                        // DB(i);
+                        // DB(sum);
+                        // DB(zero[i]);
+                        // DB(infront);
+                        ans += two[i];
+                        sum += zero[i];
+                    }
+                    else
+                    {
+                        sum += one[i];
+                    }
+                }
+                System.out.println("Case #" + cs + ": " + ans);
+            }
+        }
+        //        try (Scanner sc = new Scanner(System.in))
+        //        {
+        //            int t = sc.nextInt();
+        //            for (int cs = 1; cs <= t; ++cs)
+        //            {
+        //                int n = sc.nextInt();
+        //                long m = sc.nextLong();
+        //                List<Long> list = new ArrayList<>();
+        //                for (int i = 0; i < n; ++i)
+        //                {
+        //                    list.add(sc.nextLong());
+        //                }
+        //                int max = -1;
+        //                long maxValue = -1;
+        //                for (Long val : list){
+        //                    maxValue = Math.max(maxValue, val);
+        //                }
+        //                maxValue = Math.max(maxValue, m);
+        //                for (int i = 0; i <= maxValue * 2; ++i)
+        //                {
+        //                    long value = 0;
+        //                    for (int j = 0; j < n; ++j)
+        //                    {
+        //                        value += list.get(j) ^ i;
+        //                    }
+        //                    if (value <= m)
+        //                        max = Math.max(max, i);
+        //                }
+        //                list.clear();
+        //                System.out.println("Case #" + cs + ": " + max);
+        //            }
+        //        }
+    }
 }
